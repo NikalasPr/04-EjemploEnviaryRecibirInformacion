912588b748d9c9dcff1c71fa0a2d01d5d3de535c
finni <nikalas.boyanov.nunev@cieep.com>
2023-09-26 13:52:03 +0200

    Segunda parte terminada, envia info

--- app/src/main/java/nikalas/nunev/a04_ejemploenviaryrecibirinformacion/MainActivity.java
+++ app/src/main/java/nikalas/nunev/a04_ejemploenviaryrecibirinformacion/MainActivity.java
@@ -1,5 +1,10 @@
 package nikalas.nunev.a04_ejemploenviaryrecibirinformacion;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -9,12 +14,19 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import nikalas.nunev.a04_ejemploenviaryrecibirinformacion.modelos.Direccion;
 import nikalas.nunev.a04_ejemploenviaryrecibirinformacion.modelos.Usuario;
 
 public class MainActivity extends AppCompatActivity {
     private EditText txtEmail;
     private EditText txtPassword;
     private Button btnDesencriptar;
+    private Button btnCrearDireccion;
+
+    //private final int DIRECCIONES = 123;
+    //por cada actividad es un laucher individual
+    private ActivityResultLauncher<Intent> launcherDirecciones;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +34,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         inicializarVista();
+        inicializarLaunches();
 
         btnDesencriptar.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -49,11 +62,70 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         });
+
+        btnCrearDireccion.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this,CrearDireccionActivity.class);
+                //startActivityForResult(intent, DIRECCIONES);
+                launcherDirecciones.launch(intent);
+            }
+        });
     }
+
+    private void inicializarLaunches() {
+        //1. preparar como lanzar la actividad hija (equivalente a startActivityForResult())
+        //2. preparar que voy a hacer cuando la hija devuelva datos (equivalente al onActivityResult())
+        launcherDirecciones = registerForActivityResult(
+                new ActivityResultContracts.StartActivityForResult(),
+                new ActivityResultCallback<ActivityResult>() {
+                    @Override
+                    public void onActivityResult(ActivityResult result) {
+                        if (result.getResultCode() == RESULT_OK){
+                            if (result.getData() != null){
+                                Bundle bundle = result.getData().getExtras();
+                                Direccion direccion = (Direccion) bundle.getSerializable("DIR");
+                                Toast.makeText(MainActivity.this, direccion.toString(), Toast.LENGTH_SHORT).show();
+                            }else {
+                                Toast.makeText(MainActivity.this, "No hay datos", Toast.LENGTH_SHORT).show();
+                            }
+                        }else {
+                            Toast.makeText(MainActivity.this, "Cancelada", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                }
+        );
+    }
+    //para que salga eso, /** y enter
+    /*
+     *
+     * @param requestCode -> id de la ventana que se ha cerrado
+     * @param resultCode -> el modo en el que se ha cerrado
+     * @param data -> datos que estan dentro del intent
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == DIRECCIONES){
+            if (resultCode == RESULT_OK){
+                if (data != null){
+                    Bundle bundle = data.getExtras();
+                    Direccion direccion = (Direccion) bundle.getSerializable("DIR");
+                    Toast.makeText(this, direccion.toString(), Toast.LENGTH_SHORT).show();
+                }else {
+                    Toast.makeText(this, "No hay datos", Toast.LENGTH_SHORT).show();
+                }
+            }else {
+                Toast.makeText(this, "Cancelada", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+     */
 
     private void inicializarVista() {
         txtEmail = findViewById(R.id.txtEmailMain);
         txtPassword = findViewById(R.id.txtPasswordMain);
         btnDesencriptar = findViewById(R.id.btnDesencriptarMain);
+        btnCrearDireccion = findViewById(R.id.btnCrearDireccionMain);
     }
 }
